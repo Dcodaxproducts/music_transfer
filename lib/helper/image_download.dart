354a5e0f408433d362c0e61717e693e0ad8bd07b
Muhammad Shafique <shafiquecbl@gmail.com>
2025-01-24 10:21:17 +0500

    android 1.8(11) - iOS 2.7(1)

--- lib/helper/image_download.dart
+++ lib/helper/image_download.dart
@@ -4,18 +4,16 @@ import 'package:matrix_ai/imports.dart';
 class DownloadImage {
   static downloadImage(String url) async {
     showLoading();
-    bool success = (await GallerySaver.saveImage(url, albumName: AppConstants.APP_NAME)) ?? false;
-    if (success) {
-      showToast('image_download_success'.tr, success: true);
-    } else {
-      showToast('image_download_fail'.tr);
+    try {
+      bool success = (await GallerySaver.saveImage(url, albumName: AppConstants.APP_NAME)) ?? false;
+      if (success) {
+        showToast('image_download_success'.tr, success: true);
+      } else {
+        showToast('image_not_available'.tr);
+      }
+    } catch (e) {
+      showToast('image_not_available'.tr);
     }
-    // var directory = await getTemporaryDirectory();
-    // // save image
-    // String fileName = url.substring(url.lastIndexOf("/") + 1, url.length);
-    // File image = File('${directory.path}/$fileName.png');
-    // await image.writeAsBytes(bytes!);
     dismiss();
-    // Share.shareXFiles([XFile(image.path)]);
   }
 }
