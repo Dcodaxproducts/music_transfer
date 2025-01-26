caa9cf8170a1bebbb15be6cec0464bb049527237
Muhammad Shafique <shafiquecbl@gmail.com>
2025-01-26 13:50:55 +0500

    android 1.9(12) - iOS 2.7(1)

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
@@ -32,8 +31,7 @@ void main() async {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // initialize firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // Load the .env file
-  await dotenv.load();
+
   // initialize localization
   Map<String, Map<String, String>> languages = await di.init();
   // request permission for firebase messaging
