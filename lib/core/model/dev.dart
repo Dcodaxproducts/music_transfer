9621059dd95e7ab082acd6446cbe1c7a0495cccd
Muhammad Shafique <shafiquecbl@gmail.com>
2026-02-05 15:48:19 +0500

    Change default environment to production

--- lib/core/model/dev.dart
+++ lib/core/model/dev.dart
@@ -2,7 +2,7 @@ enum Env { dev, prod }
 
 class Environment {
   // TODO: change to prod before release
-  static Env environment = Env.dev;
+  static Env environment = Env.prod;
 
   static bool get isDev => environment == Env.dev;
   static bool get isProd => environment == Env.prod;
